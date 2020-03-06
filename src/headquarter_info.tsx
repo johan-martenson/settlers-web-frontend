3b19746df9adbbb2fd5e44a209195b820d7a9ff5
Johan Mårtensson O <johan.o.martensson@ericsson.com>
2020-03-06 16:23:35 +0100

    Fixed the drawing of the inventory

--- src/headquarter_info.tsx
+++ src/headquarter_info.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import { GameId, getHouseInformation, HouseInformation, PlayerId } from './api';
+import { Material, GameId, getHouseInformation, HouseInformation, PlayerId } from './api';
+import './headquarter_info.css'
 
 interface HeadquarterInfoProps {
     house: HouseInformation
@@ -30,35 +31,54 @@ class HeadquarterInfo extends Component<HeadquarterInfoProps, HeadquarterInfoSta
 
         const house = await getHouseInformation(this.props.house.id, this.props.playerId, this.props.gameId);
 
-        console.info("Got house information " + JSON.stringify(house));
+        console.info("Got house information ");
+        console.info(house);
+
+        const inventory: Map<Material, number> = new Map()
+
+        house.resources.forEach(
+            (value, key) => {
+
+                const hasAmount = value.get("has")
+
+                if (hasAmount) {
+                    inventory.set(key, hasAmount)
+                } else {
+                    inventory.set(key, 0)
+                }
+            }
+        )
 
         this.setState({
-            inventory: house.inventory
+            inventory: inventory
         });
     };
 
     render() {
 
+        if (this.state.inventory) {
+            this.state.inventory.keys()
+        }
+
+        const inventoryItems = []
+
+        let index = 0
+        for (const [material, amount] of this.state.inventory) {
+
+            index = index + 1
+
+            inventoryItems.push(
+                <span className="InventoryLabelValuePair" key={index} >
+                    <div className="InventoryLabel">{material.replace(/_/g, ' ')}</div>
+                    <div className="InventoryValue">{amount}</div>
+                </span>
+            )
+        }
+
         return (
             <div>
                 <div className="InventoryList">
-                    {Object.keys(this.state.inventory).map(
-                        (key, index) => {
-
-                            if (this.state.page * this.state.itemsPerPage <= index &&
-                                index < (this.state.page + 1) * this.state.itemsPerPage) {
-                                return (
-                                    <span className="InventoryLabelValuePair" key={index} >
-                                        <div className="InventoryLabel">{key.replace(/_/g, ' ')}</div>
-                                        <div className="InventoryValue">{this.state.inventory.get(key)}</div>
-                                    </span>
-                                );
-                            } else {
-                                return null;
-                            }
-                        }
-                    )
-                    }
+                    {inventoryItems}
                 </div>
 
                 {this.state.page > 0 &&

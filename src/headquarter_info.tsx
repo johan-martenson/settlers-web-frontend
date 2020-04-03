df64181380c3cc5bce49a888dc19d4f025ba02d8
Johan Mårtensson O <johan.o.martensson@ericsson.com>
2020-04-03 16:10:31 +0200

    Add periodic refreshes while the page is open.

--- src/headquarter_info.tsx
+++ src/headquarter_info.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Material, GameId, getHouseInformation, HouseInformation, PlayerId } from './api';
+import { Material, GameId, getHouseInformation, HouseInformation, PlayerId, isMaterial } from './api';
 import './headquarter_info.css'
 
 interface HeadquarterInfoProps {
@@ -13,9 +13,12 @@ interface HeadquarterInfoState {
     inventory: Map<string, number>
     page: number
     itemsPerPage: number
+    house: HouseInformation
 }
 
 class HeadquarterInfo extends Component<HeadquarterInfoProps, HeadquarterInfoState> {
+
+    private periodicUpdates: NodeJS.Timeout | null = null
 
     constructor(props: HeadquarterInfoProps) {
         super(props);
@@ -23,28 +26,45 @@ class HeadquarterInfo extends Component<HeadquarterInfoProps, HeadquarterInfoSta
         this.state = {
             inventory: new Map(),
             page: 0,
-            itemsPerPage: this.props.itemsPerPage ? this.props.itemsPerPage : 10
+            itemsPerPage: this.props.itemsPerPage ? this.props.itemsPerPage : 10,
+            house: props.house
         };
     }
 
     async componentDidMount() {
 
-        const house = await getHouseInformation(this.props.house.id, this.props.playerId, this.props.gameId);
+        console.info("House information is " + JSON.stringify(this.state.house));
 
-        console.info("Got house information ");
-        console.info(house);
+        this.updateInventory(this.state.house)
 
+        this.periodicUpdates = setInterval(async () => {
+            const house = await getHouseInformation(this.props.house.id, this.props.playerId, this.props.gameId);
+            console.info("UPDATING HOUSE")
+            this.updateInventory(house)
+        }, 1000)
+    };
+
+    componentWillUnmount() {
+        if (this.periodicUpdates) {
+            clearInterval(this.periodicUpdates)
+        }
+    }
+
+    updateInventory(house: HouseInformation): void {
         const inventory: Map<Material, number> = new Map()
 
-        house.resources.forEach(
-            (value, key) => {
+        Object.entries(house.resources).forEach(
+            ([material, hasAndNeeds], index) => {
+                const hasAmount = hasAndNeeds.has
 
-                const hasAmount = value.get("has")
+                if (!isMaterial(material)) {
+                    return
+                }
 
                 if (hasAmount) {
-                    inventory.set(key, hasAmount)
+                    inventory.set(material, hasAmount)
                 } else {
-                    inventory.set(key, 0)
+                    inventory.set(material, 0)
                 }
             }
         )
@@ -52,7 +72,9 @@ class HeadquarterInfo extends Component<HeadquarterInfoProps, HeadquarterInfoSta
         this.setState({
             inventory: inventory
         });
-    };
+
+
+    }
 
     render() {
 

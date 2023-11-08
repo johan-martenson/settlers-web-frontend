6990c6dce0b68c592f202ff30a4dd0103b86c388
Johan Mårtensson <johan.martenson@gmail.com>
2023-11-08 10:31:33 +0100

    Rewrote the music player as a functional component.

--- src/music_player.tsx
+++ src/music_player.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Button } from '@fluentui/react-components'
 import ExpandCollapseToggle from './expand_collapse_toggle'
 import './music_player.css'
@@ -6,146 +6,124 @@ import { FastForward24Filled, Pause24Filled, Play24Filled } from '@fluentui/reac
 
 interface MusicPlayerProps { }
 
-interface MusicPlayerState {
-    expanded: boolean
-    currentSong: number
-    playing: boolean
-}
-
 interface AudioAtlasSongs {
     path: string
     title: string
 }
 
-class MusicPlayer extends Component<MusicPlayerProps, MusicPlayerState> {
+const MusicPlayer = ({ }: MusicPlayerProps) => {
 
-    private audioAtlasSongs?: AudioAtlasSongs[]
-    private audioInstances: HTMLAudioElement[]
+    const [expanded, setExpanded] = useState<boolean>(false)
+    const [playing, setPlaying] = useState<boolean>(false)
+    const [songs, setSongs] = useState<HTMLAudioElement[]>([])
+    const [currentSong, setCurrentSong] = useState<number>(0)
 
-    constructor(props: MusicPlayerProps) {
-        super(props)
+    useEffect(() => {
+        (async () => {
+            const result = await fetch('assets/audio-atlas-music.json')
 
-        this.audioInstances = []
+            const audioAtlasSongs: AudioAtlasSongs[] = await result.json()
 
-        this.state = {
-            expanded: false,
-            currentSong: 0,
-            playing: false
+            const newSongs = audioAtlasSongs?.map(song => new Audio(song.path))
+
+            setSongs(newSongs)
+        })().then()
+
+        return () => { }
+    }, [])
+
+    function pause() {
+        if (songs) {
+            songs[currentSong].pause()
+
+            setPlaying(false)
         }
     }
 
-    async componentDidMount() {
-        const result = await fetch('assets/audio-atlas-music.json')
+    function resume() {
+        if (songs) {
+            songs[currentSong].onended = () => { next() }
+            songs[currentSong].play()
 
-        this.audioAtlasSongs = await result.json()
-
-        this.audioAtlasSongs?.forEach(song => this.audioInstances.push(new Audio(song.path)))
-    }
-
-    play(): void {
-        if (this.audioAtlasSongs) {
-            this.audioInstances[this.state.currentSong].onended = () => { this.next() }
-            this.audioInstances[this.state.currentSong].play()
-
-            this.setState({ playing: true })
+            setPlaying(true)
         }
     }
 
-    pause(): void {
-        if (this.audioAtlasSongs) {
-            this.audioInstances[this.state.currentSong].pause()
-
-            this.setState({ playing: false })
+    function next() {
+        if (playing) {
+            songs[currentSong].pause()
         }
+
+        const newSong = (currentSong < songs.length - 1) ? currentSong + 1 : 0
+
+        if (playing) {
+            songs[newSong].currentTime = 0
+            songs[newSong].onended = () => { next() }
+            songs[newSong].play()
+        }
+
+        setCurrentSong(newSong)
     }
 
-    next(): void {
-        if (this.state.playing) {
-            this.audioInstances[this.state.currentSong].pause()
+    function play(index: number): void {
+        if (playing) {
+            songs[currentSong].pause()
         }
 
-        const currentSong = this.state.currentSong
-        let newSong
+        songs[index].currentTime = 0
+        songs[index].onended = () => { next() }
+        songs[index].play()
 
-        if (currentSong < this.audioInstances.length - 1) {
-            newSong = currentSong + 1
-        } else {
-            newSong = 0
-        }
-
-        if (this.state.playing) {
-            this.audioInstances[newSong].currentTime = 0
-            this.audioInstances[newSong].onended = () => { this.next() }
-            this.audioInstances[newSong].play()
-        }
-
-        this.setState({ currentSong: newSong })
+        setPlaying(true)
+        setCurrentSong(index)
     }
 
-    playSong(newSong: number): void {
-        if (this.state.playing) {
-            this.audioInstances[this.state.currentSong].pause()
-        }
+    return (
+        <div className="music-player">
 
-        this.audioInstances[newSong].currentTime = 0
-        this.audioInstances[newSong].onended = () => { this.next() }
-        this.audioInstances[newSong].play()
+            <ExpandCollapseToggle onExpand={() => setExpanded(true)} onCollapse={() => setExpanded(false)} inverted />
+            <div> <b>Music</b></div>
 
-        this.setState({
-            currentSong: newSong,
-            playing: true
-        })
-    }
+            {playing && songs && expanded &&
+                <div> Playing: {songs[currentSong].title} </div>
+            }
 
-    render() {
+            {expanded &&
+                <>
+                    <div>
+                        {playing &&
+                            <span><Button onClick={() => pause()} icon={<Pause24Filled />} appearance='transparent' /></span>
+                        }
 
-        return (
-            <div className="music-player">
+                        {!playing &&
+                            <span><Button onClick={() => resume()} icon={<Play24Filled />} appearance='transparent' /></span>
+                        }
 
-                <ExpandCollapseToggle onExpand={() => this.setState({ expanded: true })} onCollapse={() => this.setState({ expanded: false })} inverted />
-                <div> <b>Music</b></div>
+                        <span><Button onClick={() => next()} icon={<FastForward24Filled />} appearance='transparent' /></span>
+                    </div>
 
-                {this.state.playing && this.audioAtlasSongs && this.state.expanded &&
-                    <div> Playing: {this.audioAtlasSongs[this.state.currentSong].title} </div>
-                }
+                    <div id="SongList">
 
-                {this.state.expanded &&
-                    <>
-                        <div>
-                            {this.state.playing &&
-                                <span><Button onClick={this.pause.bind(this)} icon={<Pause24Filled />} appearance='transparent'/></span>
-                            }
+                        {songs &&
+                            songs.map(
+                                (song, index) => {
 
-                            {!this.state.playing &&
-                                <span><Button onClick={this.play.bind(this)} icon={<Play24Filled/>} appearance='transparent'/></span>
-                            }
-
-                            <span><Button onClick={this.next.bind(this)} icon={<FastForward24Filled />} appearance='transparent'/></span>
-                        </div>
-
-                        <div id="SongList">
-
-                            {this.audioAtlasSongs &&
-                                this.audioAtlasSongs.map(
-                                    (song, index) => {
-
-                                        return (
-                                            <div className={(index === this.state.currentSong) ? "PlayingSongItem" : "SongItem"}
-                                                key={index}
-                                                onClick={() => { this.playSong(index) }}
-                                            >
-                                                {song.title}
-                                            </div>
-                                        )
-                                    }
-                                )
-                            }
-                        </div>
-                    </>
-                }
-            </div>
-        )
-    }
+                                    return (
+                                        <div className={(index === currentSong) ? "PlayingSongItem" : "SongItem"}
+                                            key={index}
+                                            onClick={() => { play(index) }}
+                                        >
+                                            {song.title}
+                                        </div>
+                                    )
+                                }
+                            )
+                        }
+                    </div>
+                </>
+            }
+        </div>
+    )
 }
 
 export default MusicPlayer
